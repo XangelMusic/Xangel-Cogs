ee8df18759767f5e6f0a36b1bde19b78da732534
XangelMusic <xangel.music@gmail.com>
2021-04-06 00:13:41 +0800

    update

--- picturesauce/picturesauce.py
+++ picturesauce/picturesauce.py
@@ -8,4 +8,4 @@ class Picturesauce(commands.Cog):
     async def mycom(self, ctx):
         """This does stuff!"""
         # Your code will go here
-        await ctx.send("I can do stuff!")
+        await ctx.send("I can do stuff test!")
